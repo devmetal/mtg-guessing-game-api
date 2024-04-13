24634418b27d349d709ae90539c7e8da3c9d2d17
devmetal <devmetal91@gmail.com>
2024-04-13 23:55:37 +0200

    ✅

--- src/auth/auth.spec.ts
+++ src/auth/auth.spec.ts
@@ -1,6 +1,9 @@
 import { test, expect, beforeAll, describe } from "bun:test";
 import { app } from "@/index";
 import { createUser } from "./service";
+import { rest } from "@/test/helper";
+
+const register = rest(app)("/api/auth/register");
 
 beforeAll(async () => {
   await createUser("test@test.com", "12345");
@@ -8,16 +11,15 @@ beforeAll(async () => {
 
 describe("POST /api/auth/register", () => {
   test("with valid datas", async () => {
-    const response = await app.request("/api/auth/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email: "test1@test.com", password: "12345" }),
+    const response = await register("POST", {
+      email: "test1@test.com",
+      password: "12345",
     });
 
     expect(response.status).toBe(200);
   });
+
+  test("with invalid datas", async () => {});
 });
 
 describe("POST /api/auth/login", () => {
