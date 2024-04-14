937bff4ea69c2ddd71e98188ec13f1a79093cfe4
devmetal <devmetal91@gmail.com>
2024-04-14 16:35:32 +0200

    ⬆️ add all auth test

--- src/test/helper.ts
+++ src/test/helper.ts
@@ -3,12 +3,16 @@ import { AppType } from "@/index";
 export const rest =
   (app: AppType) =>
   (url: string) =>
+  (headers: HeadersInit | null) =>
   (method: string, body: unknown = null) => {
     const init: RequestInit = { method };
+
+    init.headers = headers ?? {};
 
     if (body) {
       init.headers = {
         "Content-Type": "application/json",
+        ...init.headers,
       };
 
       init.body = JSON.stringify(body);
